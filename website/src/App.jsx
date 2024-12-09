15c2eec81afa50c0a0a31526205d1ea354bccb28
Mutombe <simbarashemutombe1@gmail.com>
2024-12-09 04:59:42 +0200

    ...

--- website/src/App.jsx
+++ website/src/App.jsx
@@ -2,10 +2,23 @@
 import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
 import Layout from './components/layout/layout';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
+
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Layout>
         <AppRoutes />
       </Layout>
